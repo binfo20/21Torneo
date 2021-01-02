d60fc64036085495518bede0e75a150e28a5fb15
Cristian Alioto <jokercris18@gmail.com>
2021-01-02 16:36:21 +0100

    Array di bottoni

--- demo/JavaFXApplication1/src/javafxapplication1/Controller.java
+++ demo/JavaFXApplication1/src/javafxapplication1/Controller.java
@@ -1,44 +1,34 @@
+
 package javafxapplication1;
 
 import javafx.scene.control.Button;
 
 public class Controller {
  
-    public Button btn1;
-    public Button btn2;
-    public Button btn3;
-    public Button btn4;
-    public Button btn5;
-    public Button btn6;
-    public Button btn7;
-    public Button btn8;
-    public Button btn9;
-    public Button btn10;
-    public Button btn11;
-    public Button btn12;
-    public Button btn13;
-    public Button btn14;
-    public Button btn15;
-    public Button btn16;
-    public Button btn17;
-    public Button btn18;
-    public Button btn19;
-    public Button btn20;
-    public Button btn21;
-    public Button btn22;
-    public Button btn23;
-    public Button btn24;
-    public Button btn25;
-    public Button btn26;
-    public Button btn27;
-    public Button btn28;
-    public Button btn29;
-    public Button btn30;
-    public Button btn31;
+    public Button[] array;
     
+    public Button btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn10,
+            btn11, btn12, btn13, btn14, btn15, btn16, btn17, btn18, btn19,
+            btn20, btn21, btn22, btn23, btn24, btn25, btn26, btn27, btn28,
+            btn29, btn30, btn31;
+    
+    public void inizializza(){
+        array = new Button[31];
+        array[0] = btn1;    array[10] = btn11;  array[20] = btn21;  
+        array[1] = btn2;    array[11] = btn12;  array[21] = btn22;
+        array[2] = btn3;    array[12] = btn13;  array[22] = btn23;
+        array[3] = btn4;    array[13] = btn14;  array[23] = btn24;
+        array[4] = btn5;    array[14] = btn15;  array[24] = btn25;
+        array[5] = btn6;    array[15] = btn16;  array[25] = btn26;
+        array[6] = btn7;    array[16] = btn17;  array[26] = btn27;
+        array[7] = btn8;    array[17] = btn18;  array[27] = btn28;
+        array[8] = btn9;    array[18] = btn19;  array[28] = btn29;
+        array[9] = btn10;   array[19] = btn20;  array[29] = btn30;
+        array[30] = btn31;
+    }
     
     public void b1(){
-        btn17.setText(btn1.getText());
+        btn17.setText(array[0].getText());
     }
     public void b2(){
         btn17.setText(btn2.getText());
@@ -127,23 +117,11 @@ public class Controller {
     public void b30(){
         btn31.setText(btn30.getText());
     }
+    
     public void reset(){
-        btn17.setText("");
-        btn18.setText("");
-        btn19.setText("");
-        btn20.setText("");
-        btn21.setText("");
-        btn22.setText("");
-        btn23.setText("");
-        btn24.setText("");
-        btn25.setText("");
-        btn26.setText("");
-        btn27.setText("");
-        btn28.setText("");
-        btn29.setText("");
-        btn30.setText("");
-        btn31.setText("");
-       
+        for(int i = 16; i<31; i++){
+            array[i].setText("");
+        }
     }
     
 }
