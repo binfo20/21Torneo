ae3dd016b52b8526cbf50dc06fd470cc94e4d2f9
memelotti <giampietromelotti64@gmail.com>
2021-01-08 16:20:35 +0100

    Create giocatori.java

---
+++ demo/JavaFXApplication1/src/javafxapplication1/giocatori.java
@@ -0,0 +1,16 @@
+package Torneo21;
+
+public class giocatori {
+	import java.util.*; 
+	Scanner sc = new Scanner(System.in);
+	
+	public String nome;
+	public int num
+	
+	ArrayList<String> list=new ArrayList<String>();
+	System.out.println("Inserisci i nomi dei giocatori");
+	for(i=0; i<1; i++) {
+		Arraylist[i]= sc.nextInt();
+	}
+	
+}
